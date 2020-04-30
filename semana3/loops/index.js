b646906c99e1a4569ea417575f00346b868dd565
Alex Quadros <aj-silva@hotmail.com>
2020-04-29 23:20:00 -0300

    semana3-aula11-atualizada

--- semana3/loops/index.js
+++ semana3/loops/index.js
@@ -53,16 +53,78 @@ __proto__: Array(0) */
   length: 1
 __proto__: Array(0) */
 
-/* ECERCÍCIO 3 */
-/* a. Escreva um programa que devolva o maior e o menor números contidos no array original */
+/* ECERCÍCIO 3
+
+a. Escreva um programa que devolva o maior e menor
+números contidos no array original */
 let array = [80, 30, 130, 40, 60, 21, 70, 120, 90, 103, 110, 55]
 let maxNumeroAtual = array[0]
+let minNumeroAtual = array[0]
 
 for (let i = 0; i < array.length; i++) {
-  if (array[i] > maxNumeroAtual) {
+  if (array[i] > maxNumeroAtual) 
     maxNumeroAtual = array[i]
+    
+  if (array[i] < minNumeroAtual) 
+    minNumeroAtual = array[i]
+  
   }
-}
 
-console.log(maxNumeroAtual)
-// Resposta no console "130"
+console.log("O maior numero é", maxNumeroAtual, "e o menor é", minNumeroAtual)
+// Resposta no console "O maior numero é 130 e o menor é 21"
+
+/* b. Escreva um programa que devolva um novo array contendo todos os valores do array original divididos por 10. */
+
+let novoarray = []
+
+for (let numero of array) {
+    novoarray.push(numero/10)  
+  } 
+  
+  console.log(novoarray)
+// Resposta no console "[8, 3, 13, 4, 6, 2.1, 7, 12, 9, 10.3, 11, 5.5]"
+
+/* c. Escreva um programa que devolva um novo array contendo, somente, os números pares do array original*/
+
+let pararray = []
+
+for (let numero of array) {
+    if (numero%2 === 0) {
+      pararray.push(numero)      
+      }     
+  }   
+  console.log(pararray)
+  // Resposta no console "[80, 30, 130, 40, 60, 70, 120, 90, 110]"
+
+/* d. Escreva um programa que gere um novo array contendo strings, da seguinte forma: "O elemento do índex i é: numero" */
+let tamanhoDoArray = array.length
+
+console.log("O tamanho do elemento é", tamanhoDoArray)
+
+console.log("O elemento do index 0 é", array[0])
+console.log("O elemento do index 1 é", array[1])
+console.log("O elemento do index 2 é", array[2])
+console.log("O elemento do index 3 é", array[3])
+console.log("O elemento do index 4 é", array[4])
+console.log("O elemento do index 5 é", array[5])
+console.log("O elemento do index 6 é", array[6])
+console.log("O elemento do index 7 é", array[7])
+console.log("O elemento do index 8 é", array[8])
+console.log("O elemento do index 9 é", array[9])
+console.log("O elemento do index 10 é", array[10])
+console.log("O elemento do index 11 é", array[11])
+/* Resposta no console:
+O tamanho do elemento é 12
+index.js:102 O elemento do index 0 é 80
+index.js:103 O elemento do index 1 é 30
+index.js:104 O elemento do index 2 é 130
+index.js:105 O elemento do index 3 é 40
+index.js:106 O elemento do index 4 é 60
+index.js:107 O elemento do index 5 é 21
+index.js:108 O elemento do index 6 é 70
+index.js:109 O elemento do index 7 é 120
+index.js:110 O elemento do index 8 é 90
+index.js:111 O elemento do index 9 é 103
+index.js:112 O elemento do index 10 é 110
+index.js:113 O elemento do index 11 é 55
+*/
